8a9f1481af5d31267658e3ced6bcd6afa3b39ed2
choosenname <forspum2@gmail.com>
2022-12-04 20:37:36 +0300

    13.2

--- ConsoleApp2/КПиЯП.cs
+++ ConsoleApp2/КПиЯП.cs
@@ -8,17 +8,47 @@ namespace ConsoleApp2
 
         public static void Main(string[] args)
         {
-            IntervalFunc(Math.Cos);
-            Console.WriteLine("-----------");
-            IntervalFunc(Math.Cosh);
-        }
+            /*DoubleDifference @double = new DoubleDifference(3.8, 5.2);
+            Console.WriteLine(@double.GetDifference());
 
-        static void IntervalFunc(Func f)
-        {
-            for (double i = 0; i < 3; i += 0.02)
+            DoubleDifferenceWithX doubleDifference = new DoubleDifferenceWithX(@double.B, 5.97, 3);
+            Console.WriteLine(doubleDifference.GetDifference());*/
+
+            Processor processor = new Processor(Processor.ProcessorModels.AMD, 2.34, 12, 783.34);
+            Console.WriteLine(processor);
+
+            processor.ShowInfo();
+
+            Motherboard motherboard = new Motherboard(processor, 12);
+            Console.WriteLine(motherboard);
+
+            Motherboard[] motherboards = new Motherboard[10];
+            motherboards[0] = motherboard;
+
+            for (int i = 0; i < motherboards.Length; i++)
             {
-                Console.WriteLine(f(i));
+                motherboards[i] = new Motherboard(i);
             }
+
+            MotherboardArray array = new MotherboardArray(motherboards);
+            array.ShowArray();
+
+            Console.WriteLine("Материнская плата на процессоре AMD с самым большим количеством оперативной памяти");
+            Console.WriteLine(array.MaxMotherBoard());
+
+            Console.WriteLine("----------------");
+            Motherboard motherboard1 = (Motherboard)motherboard.Clone();
+
+            Console.WriteLine("----------------");
+            Array.Sort(motherboards);
+            foreach (var i in motherboards)
+                i.ShowInfo();
+
+            Console.WriteLine("----------------");
+            Array.Sort(motherboards, new Motherboard());
+            foreach (var i in motherboards)
+                i.ShowInfo();
+
         }
     }
 }
