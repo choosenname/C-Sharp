0d08c6f94957daf10dd362a8d534bc5f0fd8dec0
choosenname <forspum2@gmail.com>
2022-12-05 10:06:57 +0300

    ПП 14.1

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -11,19 +11,11 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            CarPark carPark = new CarPark(new Car[] {
-                new Car(4578, "Игорь Брусков", Color.Aqua, true),
-                new Car(5236, "аебров", Color.Tan, false),
-                new Car(778, "Курсед", Color.Magenta, true) });
-
-            Console.WriteLine("Найти машину с водителем Бебров " + carPark.FindCars(car => car.LastName == "Бебров"));
-
-            Console.WriteLine("Вывести все машины на парковке");
-            carPark.ShowCars(car => car.IsOnParking);
-
-            Console.WriteLine("----------");
-            carPark[2] = new Car(1488, "Боб", Color.Azure, false);
-            Console.WriteLine(carPark[2]);
+            OneDimensional arr = new OneDimensional(15);
+            Console.WriteLine(">>>" + arr.Average());
+            arr.Show();
+            arr.SortMoreThenAverage();
+            arr.Show();
         }
     }
 }
---
+++ ConsoleApp7/one-dimensional.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using SomeFunc;
+
+namespace ConsoleApp7
+{
+    internal class OneDimensional
+    {
+        int[] arr;
+
+        public OneDimensional(int n)
+        {
+            ArrayFunc.EnterArr(out arr, n);
+        }
+
+        public OneDimensional(int[] arr)
+        {
+            Arr = arr;
+        }
+
+        public int[] Arr
+        {
+            get => arr;
+            set
+            {
+                try
+                {
+                    if (value == null) throw new ArgumentNullException();
+                    arr = value;
+                }
+                catch (ArgumentNullException ex) { ShowExeption(ex); }
+                catch (Exception ex) { ShowExeption(ex); }
+            }
+        }
+
+        public void Show()
+        {
+            ArrayFunc.OutArr(arr);
+            Console.WriteLine("-----------");
+        }
+
+        public int Average()
+        {
+            int sum = 0;
+            for (int i = 0; i < Arr.Length; i++) { sum += Math.Abs(Arr[i]); }
+
+            return sum / Arr.Length;
+        }
+
+        public void SortMoreThenAverage()
+        {
+            int av = Average();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < av) continue;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j] < av) continue;
+                    if (arr[i] > arr[j])
+                    {
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                    }
+                }
+            }
+        }
+
+        public void SpecialSort()
+        {
+
+        }
+
+        public static void ShowExeption(System.Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
