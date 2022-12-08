b456828573e0792e0bb15c63b042655ea4a68980
choosenname <vlad.lukasonok0@gmail.com>
2022-12-09 01:15:17 +0300

    Добавлена генерация слов для ПП 15

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,65 +6,18 @@ namespace КПиЯП
     {
         static void Main(string[] args)
         {
-
-            OneDimensional arr;
-            while (true)
+            string[] str = new string[15];
+            Random random = new Random();
+            for (int i = 0; i < str.Length; i++)
             {
-                Console.WriteLine("Введите размер массива");
-                try
+                string tmp = null;
+                for (int j = 0; j < random.Next(3, 17); j++)
                 {
-                    int n = Convert.ToInt32(Console.ReadLine());
-                    if (n < 1) throw new ArgumentOutOfRangeException("Размер массива не может быть меньше 1");
-                    else
-                    {
-                        arr = new OneDimensional(n);
-                        break;
-                    }
+                    tmp += (char)random.Next('a', 'z');
                 }
-                catch (Exception ex) { ShowException(ex); }
-            }
-            arr.Show();
-
-            while (true)
-            {
-                try
-                {
-                    Console.WriteLine("Выберете действие 1 - Вывести массив, 2 - Найти среднее по модулю, 3 - Отсортировать массив, 4 - Обработать массив, 5 - Выход");
-                    switch (Convert.ToInt32(Console.ReadLine()))
-                    {
-                        case 1: { arr.Show(); break; }
-                        case 2: { Console.WriteLine(">>>" + arr.Average()); break; }
-                        case 3: { arr.Sort(); break; }
-                        case 4:
-                            {
-                                bool flag = true;
-                                OneDimensional.FuncPrevByNext func = null;
-                                Console.WriteLine("Какое действие вы хотите выполнить (*,/,+,-)");
-                                switch (Console.ReadLine())
-                                {
-                                    case "*": func = (a, b) => a * b; break;
-                                    case "/": func = (a, b) => a / b; break;
-                                    case "+": func = (a, b) => a + b; break;
-                                    case "-": func = (a, b) => a - b; break;
-                                    default: Console.WriteLine("Неверное значение"); flag = false; break;
-                                }
-
-                                if (flag)
-                                {
-                                    arr.PreviousByNext(func);
-                                    arr.ShowWithCurrency();
-                                }
-                                break;
-                            }
-                        case 5: return;
-                        default: { Console.WriteLine("Неверное значение, повторите ввод"); break; }
-                    }
-                }
-                catch (FormatException ex) { ShowException(ex); }
-                catch (Exception ex) { ShowException(ex); }
+                str[i] = tmp + " ";
+                Console.WriteLine(str[i]);
             }
         }
-
-        static void ShowException(Exception ex) { Console.WriteLine(ex); }
     }
 }
