04350196524592b87816d080c964c8b0e51806c8
choosenname <vlad.lukasonok0@gmail.com>
2022-12-10 00:19:33 +0300

    Подготовка к ОКР ебать его в сраку

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] nums2 = { 1, 1, 0, 0, 4, 5, 0, 10, 1, 0, 19, 120, 120 };
+
+            int first = 0;
+            int end = nums2.Length - 1;
+            int result = 0;
+
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                if (nums2[i] == 0)
+                {
+                    first = i;
+                    break;
+                }
+            }
+
+            for (int i = nums2.Length - 1; i > -1; i--)
+            {
+                if (nums2[i] == 0)
+                {
+                    end = i;
+                    break;
+                }
+            }
+
+            for(int i = first; i < end; i++)
+            {
+                result += nums2[i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
